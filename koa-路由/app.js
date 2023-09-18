18b5c1173cc46b3751d16ba3e3e8de1e9ea4c4cd
dep <1649743146@qq.com>
2023-09-18 09:40:37 +0800

    feat: 新增静态资源管理

--- koa-路由/app.js
+++ koa-路由/app.js
@@ -1,6 +1,15 @@
 const Koa = require("Koa")
 const router = require("./routes/index")
+const path = require("path")
+// 静态资源
+const static = require("koa-static")
+
 const app = new Koa()
+
+// 静态资源管理
+app.use(static(
+    path.join(__dirname, "public")
+))
 
 app.use(router.routes()).use(router.allowedMethods())
 
